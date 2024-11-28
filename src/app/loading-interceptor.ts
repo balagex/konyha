8a69562c1496c532cbe32194c1c4dd6814df0341
Csönge Balázs <csonge.balazs@entersoft.hu>
2024-11-28 15:22:43 +0100

    angular 17 -> 18 upgrade

--- src/app/loading-interceptor.ts
+++ src/app/loading-interceptor.ts
@@ -1,12 +1,6 @@
 import { LoadingService } from './loading.service';
 import { Injectable } from '@angular/core';
-import {
-    HttpRequest,
-    HttpHandler,
-    HttpEvent,
-    HttpInterceptor,
-    HTTP_INTERCEPTORS
-} from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 
